5b82dd9b60150d45069458b5166677337437ea8f
Casey Meurer <cmeurer@ionizationlabs.com>
2020-12-20 19:01:55 -0600

    add search function

--- src/components/form.js
+++ src/components/form.js
@@ -1,14 +1,18 @@
 import React from "react"
+import Spotify from "../util/Spotify"
+
 import { Button, Form } from 'semantic-ui-react'
 
-const Searchform = () => (
-  <Form>
-    <Form.Field>
-      <label>Playlist</label>
-      <input placeholder='Playlist' />
-    </Form.Field>
-    <Button type='submit'>Submit</Button>
-  </Form>
-)
+const Searchform = (props) => {
+  return (
+    <Form onSubmit={Spotify}>
+      <Form.Field>
+        <label>Playlist</label>
+        <input placeholder='Playlist' />
+      </Form.Field>
+      <Button type='submit'>Submit</Button>
+    </Form>
+  )
+}
 
 export default Searchform
