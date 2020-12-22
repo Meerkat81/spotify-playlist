f273e24dd02c2d343e8c1032956701a5b5ab3ad2
Casey Meurer <cmeurer@ionizationlabs.com>
2020-12-22 10:31:53 -0600

    calling token and playlist

--- src/util/Searches.js
+++ src/util/Searches.js
@@ -2,66 +2,45 @@ let accessToken;
 const Secret = 'Basic NzRhNDRjODI1ZGRkNDQ1N2ExMjBiMGY2MjE2ODJlMjE6YzZlNmQyZDZmOWE1NDA3Nzk4MmZlYjU1NmNkNzUzNzU='
 
 const Spotify = {
-  getAccessToken(){
-    
-    if(accessToken){
-      return accessToken
-    }
-
-    async function postData(url = '', data = {}) {
+  async postData(url = '', data = {}) {
     // Default options are marked with *
-    const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    let request = {
+      method: (data.method || 'POST'), // *GET, POST, PUT, DELETE, etc.
       mode: 'cors', // no-cors, *cors, same-origin
       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
       credentials: 'same-origin', // include, *same-origin, omit
       headers: {
         //'Content-Type': 'application/json',
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization' : Secret,
+        'Authorization' : (data.auth || Secret),
       },
       redirect: 'follow', // manual, *follow, error
       referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: new URLSearchParams(data) // body data type must match "Content-Type" header
-    });
+    };
+    if(data.method === 'GET') delete request.body
+    const response = await fetch(url, request);
     return response.json(); // parses JSON response into native JavaScript objects
-  }
-
-  postData('https://accounts.spotify.com/api/token', { grant_type: 'client_credentials' })
-  .then(data => {
+  },
+  async getAccessToken(){
+    if(accessToken){
+      return accessToken
+    }
+    let data = await Spotify.postData('https://accounts.spotify.com/api/token', { grant_type: 'client_credentials' })
     accessToken = data.access_token
     return accessToken
-  });
   },
   
-  playlistsearch(){
-    const accessToken = Spotify.getAccessToken()
+  async playlistsearch(){
+    const accessToken = await Spotify.getAccessToken()
+    console.log(accessToken)
     const header = "Bearer " + accessToken
-    const headers = {Authorization: `Bearer ${accessToken}`}
-    async function postData(url = '', data = {}) {
-    // Default options are marked with *
-      const response = await fetch(url, {
-        method: 'GET', // *GET, POST, PUT, DELETE, etc.
-        //mode: 'cors', // no-cors, *cors, same-origin
-        //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        //credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-          'Content-Type': 'application/json',
-          //'Content-Type': 'application/x-www-form-urlencoded',
-          'Authorization' : header,
-
-      },
-      //redirect: 'follow', // manual, *follow, error
-      //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      //body: new URLSearchParams(data) // body data type must match "Content-Type" header
+    
+    console.log(header)
+    Spotify.postData(`https://api.spotify.com/v1/search?q=90s&type=playlist&limit=10`, {auth: header, method: 'GET'})
+    .then(data => {
+      console.log(data)
     });
-    return response.json(); // parses JSON response into native JavaScript objects
-    }
-    console.log(headers)
-    postData(`https://api.spotify.com/v1/search?q=90s&type=playlist&limit=10`, {})
-  .then(data => {
-    console.log(data)
-  });
   },
 }
 
