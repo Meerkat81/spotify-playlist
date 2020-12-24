3414b391f1ab702f1540da931a62113c22b3a237
Casey Meurer <cmeurer@ionizationlabs.com>
2020-12-24 15:18:16 -0600

    table - initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import React from "react"
-import logo from './logo.svg'
+import React, { useState} from "react"
 import Searchform from "./components/form"
-import Spotify from "./util/Spotify"
+import Spotify from "./util/Searches"
+import Table from "./components/table"
 import './App.css'
 //Show Search Box
-//TODO: Query Spotify playlists
-//TODO: List play list
+//Get Spotify token
+//Query Spotify playlists
+//TODO: Show play list
 //TODO: List Songs of playlist
 //TODO: Break songs up by category into line graph
-
-// Define the string
-var decodedStringBtoA = '74a44c825ddd4457a120b0f621682e21:c6e6d2d6f9a54077982feb556cd75375';
-
-// Encode the String
-var encodedStringBtoA = btoa(decodedStringBtoA);
-console.log(encodedStringBtoA);
-var decoded = atob(encodedStringBtoA)
-console.log(decoded)
 
 const App = (props) => {
     return (
       <div className="App">
         <Searchform />
+        <Table />
       </div>
     )
 }
