0677ca15bb06d14bf1cebd4f0c4c912033eb47c0
Meerkat81 <57145227+Meerkat81@users.noreply.github.com>
2023-09-25 16:44:05 +0200

    use spotify module

---
+++ src/modules/spotifyApi.js
@@ -0,0 +1,61 @@
+let accessToken = false;
+const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+const CLIENT_SECRET = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
+
+const secretHash = new Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString(
+  "base64"
+);
+let secret = "Basic " + secretHash;
+
+async function postData(url = "", data = {}) {
+  // Default options are marked with *
+  let request = {
+    method: data.method || "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      //'Content-Type': 'application/json',
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: data.auth || secret,
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: new URLSearchParams(data), // body data type must match "Content-Type" header
+  };
+  if (data.method === "GET") delete request.body;
+  try {
+    const res = await fetch(url, request);
+    if (!res.ok) throw Error("Something went wrong");
+    return res.json();
+  } catch (err) {
+    console.log(err);
+    return { err };
+  } // parses JSON response into native JavaScript objects
+}
+
+async function getAccessToken() {
+  if (accessToken) return accessToken;
+  let res = await postData("https://accounts.spotify.com/api/token", {
+    grant_type: "client_credentials",
+  });
+
+  if (res.accessToken) accessToken = res.access_token;
+}
+
+async function playListSearch(term) {
+  const accessToken = await getAccessToken();
+  console.log(accessToken);
+  const header = "Bearer " + accessToken;
+  let data = await postData(
+    `https://api.spotify.com/v1/search?q=${term}&type=playlist&offset=10&limit=10`,
+    { auth: header, method: "GET" }
+  );
+  console.log("1", data);
+  if (data.err) return data.err;
+  let playlists = data.playlists.items;
+  let total = data.playlists.total;
+  return [playlists, total];
+}
+
+export { playListSearch };
