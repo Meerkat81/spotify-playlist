7db6b3ab15734f142f591abd249c16e0843dd5b8
Meerkat81 <57145227+Meerkat81@users.noreply.github.com>
2023-09-25 17:57:15 +0200

    pass setQuery

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import Searchform from "./components/form";
 import Table from "./components/table";
 import { playListSearch } from "./modules/spotifyApi";
@@ -17,64 +17,69 @@ const App = (props) => {
   const [pages, setPages] = useState(0);
   const [playlistId, setplaylistId] = useState(null);
   const [tracks, setTracks] = useState(null);
-  const [searchString, setSearchString] = useState(); //use instead of onsubmit
-  const isFirstRef = useRef(true);
-  const isFirstTrack = useRef(true);
+  const [query, setQuery] = useState(null); //use instead of onsubmit
+
   const [error, setError] = useState(null);
   const search = (value) => {
-    setSearchString(value);
+    setQuery(value);
   };
-  console.log(searchString);
   const tracklistId = (value) => {
     setplaylistId(value);
   };
 
-  useEffect(() => {
-    if (isFirstTrack.current) {
-      isFirstTrack.current = false;
-      return;
-    }
-  }, [playlistId]);
-
-  useEffect(() => {
-    async function callSpotify() {
-      try {
-        const results = await playListSearch(searchString);
-        // if (results instanceof Error) {
-        //   console.log(results);
-        console.log("19", results.Error);
-      } catch (err) {
-        console.log("lol", err);
-        setError(err);
+  useEffect(
+    function () {
+      console.log("this first run");
+      function help() {
+        console.log("first run ran again");
       }
+      help();
+    },
+    [playlistId]
+  );
 
-      //   if (playListsCall.message) throw new Error(playListsCall.error);
-      // } catch (err) {
-      //   console.log(err);
-      //   setError(err.message);
-      // }
-      // .then((list) => {
-      //   setPages(list[1]);
-      //   return list[0].filter((filterlist) => filterlist.description !== "");
-      // })
-      // .then((list) => {
-      //   setPlaylists(
-      //     list.map((element) => ({
-      //       key: element,
-      //       name: element.description,
-      //       track_count: element.tracks.total,
-      //       id: element.id,
-      //     }))
-      //   );
-      // });
-    }
-    callSpotify();
-  }, [searchString]);
+  useEffect(
+    function () {
+      async function callSpotify() {
+        try {
+          const results = await playListSearch(query);
+          // if (results instanceof Error) {
+          //   console.log(results);
+          console.log("19", results.Error);
+        } catch (err) {
+          console.log("lol", err);
+          setError(err);
+        }
+
+        //   if (playListsCall.message) throw new Error(playListsCall.error);
+        // } catch (err) {
+        //   console.log(err);
+        //   setError(err.message);
+        // }
+        // .then((list) => {
+        //   setPages(list[1]);
+        //   return list[0].filter((filterlist) => filterlist.description !== "");
+        // })
+        // .then((list) => {
+        //   setPlaylists(
+        //     list.map((element) => ({
+        //       key: element,
+        //       name: element.description,
+        //       track_count: element.tracks.total,
+        //       id: element.id,
+        //     }))
+        //   );
+        // });
+      }
+      if (query) callSpotify();
+    },
+    [query]
+  );
 
   return (
     <div className="App">
       <React.Fragment>
-        <Searchform search={search} />
+        <Searchform query={query} setQuery={setQuery} />
         <ErrorMessage messsage={error} />
         {playlists && (
           <Table
--- src/components/form.js
+++ src/components/form.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Button, Form } from "semantic-ui-react";
 
-const Searchform = ({ search }) => {
+const Searchform = ({ query, setQuery }) => {
   return (
-    <Form onSubmit={(event) => search(event.target.term.value)}>
+    <Form onSubmit={(event) => setQuery(event.target.term.value)}>
       <Form.Field>
         <label>Playlist</label>
-        <input name="term" placeholder="Playlist" />
+        <input name="term" placeholder="Search Playlists" />
       </Form.Field>
       <Button type="submit">Submit</Button>
     </Form>
