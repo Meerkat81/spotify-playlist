addc836e9d3946e071cc3087447759a07bbb269e
Meerkat81 <57145227+Meerkat81@users.noreply.github.com>
2023-10-10 18:51:45 +0200

    put paginsation in own component

--- src/components/PlayListTable.js
+++ src/components/PlayListTable.js
@@ -15,23 +15,6 @@ function PlayListTable({ playlists, pages, tracklistId }) {
       <Playlist playlists={playlists} tracklistId={tracklistId} />
       <Table.Footer>
         <PlayListTablePagination />
-        {/* <Table.Row>
-          <Table.HeaderCell>1 / {pages} </Table.HeaderCell>
-          <Table.HeaderCell colSpan="3">
-            <Menu floated="right" pagination>
-              <Menu.Item as="a" icon>
-                <Icon name="chevron left" />
-              </Menu.Item>
-              <Menu.Item as="a">1</Menu.Item>
-              <Menu.Item as="a">2</Menu.Item>
-              <Menu.Item as="a">3</Menu.Item>
-              <Menu.Item as="a">4</Menu.Item>
-              <Menu.Item as="a" icon>
-                <Icon name="chevron right" />
-              </Menu.Item>
-            </Menu>
-          </Table.HeaderCell>
-        </Table.Row> */}
       </Table.Footer>
     </Table>
   );
--- src/components/PlaylistTablePagination.js
+++ src/components/PlaylistTablePagination.js
@@ -9,10 +9,6 @@ function PlayListTablePagination({ pages }) {
           <Menu.Item as="a" icon>
             <Icon name="chevron left" />
           </Menu.Item>
-          <Menu.Item as="a">1</Menu.Item>
-          <Menu.Item as="a">2</Menu.Item>
-          <Menu.Item as="a">3</Menu.Item>
-          <Menu.Item as="a">4</Menu.Item>
           <Menu.Item as="a" icon>
             <Icon name="chevron right" />
           </Menu.Item>
