9e162329b3ffedede3029c056177bb9475342fb8
Meerkat81 <57145227+Meerkat81@users.noreply.github.com>
2023-10-17 14:44:54 +0200

    chart can change

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -3,518 +3,77 @@ import { Segment } from "semantic-ui-react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
-const series = [
-  {
-    name: "Year",
-    id: "year",
-    marker: { symbol: "circle" },
-    data: [],
-  },
-];
-const playListSongs = {
-  items: [
-    {
-      track: {
-        album: {
-          release_date: "2023-10-14",
-        },
-        popularity: 0,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2023-08-04",
-        },
-        popularity: 19,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021-12-10",
-        },
-        popularity: 5,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021-12-10",
-        },
-        popularity: 4,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021-12-10",
-        },
-        popularity: 4,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021-12-10",
-        },
-        popularity: 4,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021-12-10",
-        },
-        popularity: 4,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021-12-10",
-        },
-        popularity: 4,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2020-08-27",
-        },
-        popularity: 20,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2020-08-27",
-        },
-        popularity: 20,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2020-08-27",
-        },
-        popularity: 19,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2020-08-27",
-        },
-        popularity: 19,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2020-08-27",
-        },
-        popularity: 19,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2020-08-27",
-        },
-        popularity: 19,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2017-08-04",
-        },
-        popularity: 19,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2018-04-02",
-        },
-        popularity: 20,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2022-12-10",
-        },
-        popularity: 20,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2022-12-10",
-        },
-        popularity: 20,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2022-12-10",
-        },
-        popularity: 20,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2022-12-10",
-        },
-        popularity: 19,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2022-12-10",
-        },
-        popularity: 19,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2022-12-10",
-        },
-        popularity: 19,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2022-12-10",
-        },
-        popularity: 19,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2022-10-07",
-        },
-        popularity: 87,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2023-06-02",
-        },
-        popularity: 88,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021",
-        },
-        popularity: 19,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021",
-        },
-        popularity: 19,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021",
-        },
-        popularity: 20,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2022-12-10",
-        },
-        popularity: 6,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2022-12-10",
-        },
-        popularity: 11,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2022-12-10",
-        },
-        popularity: 5,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2022-12-10",
-        },
-        popularity: 10,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2022-12-10",
-        },
-        popularity: 10,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2022-12-10",
-        },
-        popularity: 5,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021",
-        },
-        popularity: 0,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021",
-        },
-        popularity: 1,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021",
-        },
-        popularity: 1,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021",
-        },
-        popularity: 1,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021",
-        },
-        popularity: 2,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021",
-        },
-        popularity: 0,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021",
-        },
-        popularity: 18,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021",
-        },
-        popularity: 18,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021",
-        },
-        popularity: 0,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021",
-        },
-        popularity: 0,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021",
-        },
-        popularity: 0,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021",
-        },
-        popularity: 18,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2023-08-04",
-        },
-        popularity: 19,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2023-10-14",
-        },
-        popularity: 0,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021",
-        },
-        popularity: 1,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2021",
-        },
-        popularity: 1,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2020-08-27",
-        },
-        popularity: 5,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2020-08-27",
-        },
-        popularity: 6,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2023-10-14",
-        },
-        popularity: 0,
-      },
-    },
-    {
-      track: {
-        album: {
-          release_date: "2023-08-04",
-        },
-        popularity: 19,
-      },
-    },
-  ],
-};
-
 function dateFormat(date) {
   return Number(date.substring(0, 4));
 }
-playListSongs.items.forEach((song) => {
-  series[0].data.push([
-    song.track.popularity,
-    dateFormat(song.track.album.release_date),
-  ]);
-});
-const scatterOptions = {
-  chart: {
-    type: "scatter",
-    zoomType: "xy",
-  },
-  credits: {
-    enabled: false,
-  },
-  title: {
-    text: "Tracks Popularity and Year",
-    align: "center",
-  },
-  xAxis: {
+function Chart({ playlistDetail }) {
+  const series = [
+    {
+      name: "Year",
+      id: "year",
+      marker: { symbol: "circle" },
+      data: [],
+    },
+  ];
+
+  const scatterOptions = {
+    chart: {
+      type: "scatter",
+      zoomType: "xy",
+    },
+    credits: {
+      enabled: false,
+    },
     title: {
-      text: "Popularity",
+      text: "Tracks Popularity and Year",
+      align: "center",
     },
-    labels: {
-      format: "{value}",
+    xAxis: {
+      title: {
+        text: "Popularity",
+      },
+      labels: {
+        format: "{value}",
+      },
+      startOnTick: true,
+      endOnTick: true,
+      showLastLabel: true,
     },
-    startOnTick: true,
-    endOnTick: true,
-    showLastLabel: true,
-  },
-  yAxis: {
-    title: {
-      text: "Year",
+    yAxis: {
+      title: {
+        text: "Year",
+      },
+      labels: {
+        format: "{value}",
+      },
     },
-    labels: {
-      format: "{value}",
-    },
-  },
-  plotOptions: {
-    scatter: {
-      marker: {
-        radius: 2.5,
-        symbol: "circle",
+    plotOptions: {
+      scatter: {
+        marker: {
+          radius: 2.5,
+          symbol: "circle",
+          states: {
+            hover: {
+              enabled: true,
+              lineColor: "rgb(100,100,100)",
+            },
+          },
+        },
         states: {
           hover: {
-            enabled: true,
-            lineColor: "rgb(100,100,100)",
+            marker: {
+              enabled: false,
+            },
           },
         },
-      },
-      states: {
-        hover: {
-          marker: {
-            enabled: false,
-          },
+        jitter: {
+          x: 0.005,
         },
-      },
-      jitter: {
-        x: 0.005,
       },
     },
-  },
-  series,
-};
-function Chart({ playlistDetail }) {
+    series,
+  };
+
   playlistDetail.items.forEach((song) => {
     series[0].data.push([
       song.track.popularity,
