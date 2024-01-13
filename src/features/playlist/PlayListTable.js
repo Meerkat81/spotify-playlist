d2ecab39a3d1f63b488ddbe6040ea4eec76b7638
Meerkat81 <57145227+Meerkat81@users.noreply.github.com>
2024-01-13 18:47:48 +0100

    Add table header

--- src/features/playlist/PlayListTable.js
+++ src/features/playlist/PlayListTable.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table } from "semantic-ui-react";
+import { Container, Header, Table } from "semantic-ui-react";
 import Playlist from "./Playlist";
 import PlayListTablePagination from "./PlaylistTablePagination";
 function PlayListTable({
@@ -9,22 +9,27 @@ function PlayListTable({
   onPlayListClick,
 }) {
   return (
-    <Table columns={3} celled compact singleLine={false} textAlign="left">
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell width={2}></Table.HeaderCell>
-          <Table.HeaderCell>Playlist</Table.HeaderCell>
-          <Table.HeaderCell>Track Count</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Playlist playlists={playlists} onPlayListClick={onPlayListClick} />
-      <Table.Footer>
-        <PlayListTablePagination
-          onPageDirectionClick={onPageDirectionClick}
-          pages={pages}
-        />
-      </Table.Footer>
-    </Table>
+    <>
+      <Container>
+        <Header size="large">Select a playlist</Header>
+        <Table columns={3} celled compact singleLine={false} textAlign="left">
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell width={2}></Table.HeaderCell>
+              <Table.HeaderCell>Playlist</Table.HeaderCell>
+              <Table.HeaderCell>Track Count</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Playlist playlists={playlists} onPlayListClick={onPlayListClick} />
+          <Table.Footer>
+            <PlayListTablePagination
+              onPageDirectionClick={onPageDirectionClick}
+              pages={pages}
+            />
+          </Table.Footer>
+        </Table>
+      </Container>
+    </>
   );
 }
 
