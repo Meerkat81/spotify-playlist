710a8b61d7fa10e0311336d44c40bb53b4442f4d
Meerkat81 <57145227+Meerkat81@users.noreply.github.com>
2024-01-13 19:05:01 +0100

    remove container

--- src/features/playlist/PlayListTable.js
+++ src/features/playlist/PlayListTable.js
@@ -10,25 +10,23 @@ function PlayListTable({
 }) {
   return (
     <>
-      <Container>
-        <Header size="large">Select a playlist</Header>
-        <Table columns={3} celled compact singleLine={false} textAlign="left">
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell width={2}></Table.HeaderCell>
-              <Table.HeaderCell>Playlist</Table.HeaderCell>
-              <Table.HeaderCell>Track Count</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Playlist playlists={playlists} onPlayListClick={onPlayListClick} />
-          <Table.Footer>
-            <PlayListTablePagination
-              onPageDirectionClick={onPageDirectionClick}
-              pages={pages}
-            />
-          </Table.Footer>
-        </Table>
-      </Container>
+      <Header size="large">Select a playlist</Header>
+      <Table columns={3} celled compact singleLine={false} textAlign="left">
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell width={2}></Table.HeaderCell>
+            <Table.HeaderCell>Playlist</Table.HeaderCell>
+            <Table.HeaderCell>Track Count</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Playlist playlists={playlists} onPlayListClick={onPlayListClick} />
+        <Table.Footer>
+          <PlayListTablePagination
+            onPageDirectionClick={onPageDirectionClick}
+            pages={pages}
+          />
+        </Table.Footer>
+      </Table>
     </>
   );
 }
